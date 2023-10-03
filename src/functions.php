3c0cbd01d3809b711334e7e8168cc7bdd9aeb903
Freezemage <freezemage0@gmail.com>
2023-10-03 11:05:18 +0300

    Initial commit.

---
+++ src/functions.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Freezemage\ArrayUtils;
+
+/**
+ * @param array $array
+ * @param callable(mixed $value, array-key $key): bool $constraint
+ *
+ * @return mixed
+ */
+function find(array $array, callable $constraint) {
+    foreach ($array as $key => $value) {
+        if ($constraint($value, $key) === true) {
+            return $value;
+        }
+    }
+
+    return null;
+}
